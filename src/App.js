af7533937d13d6119b3c5a65f683054a61c1d200
Charan Singh <charansinghadvance@gmail.com>
2023-09-13 13:53:06 +0530

    Initial setup for routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
+import Body from "./components/Body";
 
 function App() {
   return (
-    <div>
-       <h1 className="text-3xl font-bold underline text-green-200">
-      Hello world!
-    </h1>
-    </div>
+    <Body />
   );
 }
 
